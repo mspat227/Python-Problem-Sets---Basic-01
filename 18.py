d274ee33e06c5e0af750e258e73ca2ea2c308492
Mayura Pathirana <108686479+mspat227@users.noreply.github.com>
2023-04-10 23:15:55 +0530

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,6 +1,7 @@
 '''
 Write a Python program to test whether a number is within 100 of 1000 or 2000.
 '''
+#get the number
 number = int(input("What is the number: "))
 
 if abs(number - 1000) <= 100 or abs(number - 2000) <= 100:
