3bee55c91546480a6fdaea49eb75f49e4e8d71a5
Mayura Pathirana <108686479+mspat227@users.noreply.github.com>
2023-04-10 23:16:27 +0530

    Update calender_p.py

--- calender_p.py
+++ calender_p.py
@@ -2,7 +2,7 @@
  Write a Python program that prints the calendar for a given month and year.
 Note : Use 'calendar' module.
 '''
-
+#importing calender module
 import calendar
 
 y = int(input("What is the year: "))
