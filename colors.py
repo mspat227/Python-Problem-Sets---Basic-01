f9b2c8b3d33b2183f1586713452a8b1d1883de2d
Mayura Pathirana <108686479+mspat227@users.noreply.github.com>
2023-04-13 17:40:17 +0530

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,16 @@
+'''
+29. Write a Python program that prints out all colors from color_list_1 that are not present in color_list_2.
+
+another way can use the sets diffarance() methode
+'''
+
+color_list_1 = set(["White", "Black", "Red"])
+color_list_2 = set(["Red", "Green"])
+
+color_list_3 = set()
+
+for i in color_list_1:
+    if i not in color_list_2:
+        color_list_3.add(i)
+
+print(color_list_3)
