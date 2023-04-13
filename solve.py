10e6eb0e5b5f48fe3278c49edd3632c79b1bb35f
Mayura Pathirana <108686479+mspat227@users.noreply.github.com>
2023-04-13 22:56:35 +0530

    Add files via upload

---
+++ solve.py
@@ -0,0 +1,12 @@
+'''
+38. Write a Python program to solve (x + y) * (x + y).
+Test Data : x = 4, y = 3
+Expected Output : (4 + 3) ^ 2) = 49
+'''
+
+x = 4
+y = 3
+
+sum = (x + y) * (x + y)
+
+print(sum)
