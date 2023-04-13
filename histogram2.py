e23d03609727e20916980389c79a420ee91fcc1d
Mayura Pathirana <108686479+mspat227@users.noreply.github.com>
2023-04-13 17:41:56 +0530

    Add files via upload

---
+++ histogram2.py
@@ -0,0 +1,7 @@
+'''
+26. Write a Python program to create a histogram from a given list of integers.
+'''
+my_list = [1, 2, 3, 5 ,6, 8, 4, 2]
+
+for i in range(len(my_list)):
+    print("#" * my_list[i])
