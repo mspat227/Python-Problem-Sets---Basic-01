1f04b5b9b5ff56f6c9030d7929edfc0d20f7dd91
Mayura Pathirana <108686479+mspat227@users.noreply.github.com>
2023-04-13 17:40:42 +0530

    Add files via upload

---
+++ concatinate.py
@@ -0,0 +1,9 @@
+'''
+27. Write a Python program that concatenates all elements in a list into a string and returns it
+'''
+
+my_list = ['M', 'a', 'y', 'u', 'r', 'a']
+
+
+for i in range(len(my_list)):
+    print(my_list[i], end='')
